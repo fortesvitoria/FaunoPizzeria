d213d4c0e3fd0261d3b08c9325148261e7f3506d
Perkles <fortesvitoria@gmail.com>
2023-10-04 20:28:41 -0300

    ajusta logica

--- logica.js
+++ logica.js
@@ -1,4 +1,46 @@
-let valor;
-let finalizar = document.querySelector('#finalizar')
-let adicionar = document.querySelector("adicionar")
-const valorTotal = document.querySelector('#valorTotal')
+//criando variáveis
+
+//variavel do valor inserido no span
+let valor = 0
+
+//botoes
+let item01 = document.getElementById("item01")
+let item02 = document.getElementById("item02")
+let tabelaCorpo = document.getElementById("tabela-corpo");
+let newRow = tabelaCorpo.insertRow();
+let valor01 = 19
+let valor02 = 47
+let indice = 1
+let cell1 = newRow.insertCell(0);
+let cell2 = newRow.insertCell(1);
+
+
+
+
+item01.addEventListener("click", function () {
+    // Adicionar 1 ao valor atual
+    valor += valor01;
+    // Atualizar o conteúdo da tag de span com o novo valor
+    document.getElementById("valor").textContent = "R$ " + valor;
+
+    cell1 = newRow.insertCell(0);
+    cell2 = newRow.insertCell(1);
+    cell1.textContent = valor
+    cell2.textContent = "Pizza"
+    indice++
+
+})
+
+item02.addEventListener("click", function () {
+    // Adicionar 1 ao valor atual
+    valor += valor02;
+    // Atualizar o conteúdo da tag de span com o novo valor
+    document.getElementById("valor").textContent = "R$ " + valor;
+    cell1 = newRow.insertCell(0);
+    cell2 = newRow.insertCell(1);
+    cell1.textContent = valor
+    cell2.textContent = "Abacate"
+    indice++
+
+})
+
